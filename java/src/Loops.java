896bca033b951f80f9def961dbb741e631c36211
Krishna-agg18 <kraggr2909@gmail.com>
2026-07-20 08:21:04 +0530

    loops java

---
+++ java/src/Loops.java
@@ -0,0 +1,16 @@
+public class Loops {
+    static void main() {
+
+
+        //for loops
+        for (int i = 1; i<= 5; i++){
+            System.out.println("Hello World!");
+        }
+        for (int day = 1; day <= 5; day++) {
+            System.out.println(day);
+        }
+
+        
+
+    }
+}
