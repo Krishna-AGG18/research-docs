449092a2b3dd3d8e8effdb41c1edd19bfb5850ef
Krishna-agg18 <kraggr2909@gmail.com>
2026-08-04 00:14:44 +0530

    arrays 2d

--- java/src/Array3.java
+++ java/src/Array3.java
@@ -16,46 +16,45 @@ public class Array3 {
                 arr[i][j] = sc.nextInt();
             }
         }
-arr.
         //print arr
-        int rowlen = arr.length;
-        int collen = arr[0].length;
-        for(int i = 0; i<rowlen; i++){
-            for(int j = 0; j < collen;j++){
+        int arrrowlen = arr.length;
+        int arrcollen = arr[0].length;
+        for(int i = 0; i<arrrowlen; i++){
+            for(int j = 0; j < arrcollen;j++){
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
         //init
-//        int brr [][] = {
-//                {1,2,3,4},{5,6,7,8},{9,10,11,12}
-//        };
-//
-//        //first element
-//        System.out.println("First element : "+brr[0][0]);
-//
-//        //print entire array
-//        int rowlen = brr.length;
-//        int collen = brr[0].length;
-//        for(int i = 0; i<rowlen; i++){
-//            for(int j = 0; j < collen;j++){
-//                System.out.print(brr[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-//
-//        //jacked array
-//        int crr [][] = {
-//                {1,2},{5,6,7,8},{9,10,11}
-//        };
-//
-//        int rIndex = crr.length;
-//        for(int i = 0; i<rIndex; i++){
-//            int cIndex = crr[i].length;
-//            for(int j = 0; j < cIndex ;j++){
-//                System.out.print(brr[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
+        int brr [][] = {
+                {1,2,3,4},{5,6,7,8},{9,10,11,12}
+        };
+
+        //first element
+        System.out.println("First element : "+brr[0][0]);
+
+        //print entire array
+        int rowlen = brr.length;
+        int collen = brr[0].length;
+        for(int i = 0; i<rowlen; i++){
+            for(int j = 0; j < collen;j++){
+                System.out.print(brr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        //jacked array
+        int crr [][] = {
+                {1,2},{5,6,7,8},{9,10,11}
+        };
+
+        int rIndex = crr.length;
+        for(int i = 0; i<rIndex; i++){
+            int cIndex = crr[i].length;
+            for(int j = 0; j < cIndex ;j++){
+                System.out.print(brr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
